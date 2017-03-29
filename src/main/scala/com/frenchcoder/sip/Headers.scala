32474334f800659bba3cdeb5c49f2f9df7f0baff
Jean-Pierre Thomasset <jpthomasset@gmail.com>
2017-03-29 14:24:14 +0200

    Add Header conversion to scala class

--- src/main/scala/com/frenchcoder/sip/Headers.scala
+++ src/main/scala/com/frenchcoder/sip/Headers.scala
@@ -1,3 +1,17 @@
 package com.frenchcoder.sip
 
+import javax.sip.{header => jsh}
+import gov.nist.javax.sip.{header => gsh}
+
 class Headers(val headers: Seq[(String, String)])
+
+object Headers {
+  def apply(headers: Iterator[jsh.Header]) : Headers = {
+    new Headers(headers.map(h => headerToSeq(h.asInstanceOf[gsh.SIPHeader])).toSeq)
+  }
+
+  def headerToSeq(header: gsh.SIPHeader): (String, String) = {
+    (header.getName, header.getValue)
+  }
+
+}
--- src/main/scala/com/frenchcoder/sip/Request.scala
+++ src/main/scala/com/frenchcoder/sip/Request.scala
@@ -1,9 +1,9 @@
 package com.frenchcoder.sip
 
+import javax.sip.{ message => jsm }
 
-class Method(name: String) {
-  
-}
+
+class Method(val name: String)
 
 case object Register extends Method("REGISTER")
 case object Invite extends Method("INVITE")
@@ -17,4 +17,11 @@ class Request[A](val method: Method, val headers: Headers, val body: A) {
   
 }
 
+object Request {
+  def apply(request: jsm.Request): Request[_] = {
+    val method = new Method(request.getMethod)
+    //    val headers = request.getH
+    new Request(method, new Headers(Seq.empty), null)
+  }
+}
 
