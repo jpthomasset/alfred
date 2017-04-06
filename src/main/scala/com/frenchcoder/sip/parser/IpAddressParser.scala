bcd9126d1369c2e52d3e06e416cdfd47632e7819
Jean-Pierre Thomasset <jpthomasset@gmail.com>
2017-04-06 13:44:44 +0200

    Refactor TelParser & add IpAddressParser

---
+++ src/main/scala/com/frenchcoder/sip/parser/IpAddressParser.scala
@@ -0,0 +1,34 @@
+package com.frenchcoder.sip.parser
+
+import org.parboiled2._
+
+trait IpAddressParser { this: Parser =>
+  import CharPredicate.{Alpha, Digit, HexDigit}
+
+  def IpV6Address: Rule0 = rule {
+                                                   6.times(H16 ~ ":") ~ LS32 |
+                                            "::" ~ 5.times(H16 ~ ":") ~ LS32 |
+          optional(H16) ~                   "::" ~ 4.times(H16 ~ ":") ~ LS32 |
+    optional((1 to 2).times(H16).separatedBy(":"))  ~ "::" ~ 3.times(H16 ~ ":") ~ LS32 |
+    optional((1 to 3).times(H16).separatedBy(":"))  ~ "::" ~ 2.times(H16 ~ ":") ~ LS32 |
+    optional((1 to 4).times(H16).separatedBy(":"))  ~ "::" ~         H16 ~ ":"  ~ LS32 |
+    optional((1 to 5).times(H16).separatedBy(":"))  ~ "::" ~                      LS32 |
+    optional((1 to 6).times(H16).separatedBy(":"))  ~ "::" ~                      H16  |
+    optional((1 to 7).times(H16).separatedBy(":"))  ~ "::"
+  }
+
+  def H16 = rule { (1 to 4).times(HexDigit) }
+
+  def LS32 = rule { (H16 ~ ":" ~ H16) | IpV4Address }
+
+  def IpV4Address = rule { 3.times(DecOctet ~ ".") ~ DecOctet }
+
+
+  def DecOctet = rule {
+    "1"         ~ Digit       ~ Digit |
+    "2"         ~ ("0" - "4") ~ Digit |
+    "25"        ~ ("0" - "5")         |
+    ("1" - "9") ~ Digit               |
+    Digit
+  }
+}
