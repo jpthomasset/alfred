a3b53aab861d42155cd8083c65067fe47906e3f9
Jean-Pierre Thomasset <jpthomasset@gmail.com>
2017-04-11 06:57:51 +0200

    Add uri-parameters parsing

--- src/main/scala/com/frenchcoder/sip/parser/Parser.scala
+++ src/main/scala/com/frenchcoder/sip/parser/Parser.scala
@@ -20,6 +20,19 @@ class MessageParser(val input: ParserInput) extends Parser with TelParser with I
   def port = rule { oneOrMore(CharPredicate.Digit) }
   def host = rule { domain | IpV4Address | IpV6Address }
   def hostAndPort = rule { host ~ optional(':' ~ port) }
+  def token = rule { oneOrMore(CharPredicate.AlphaNum | CharPredicate("-.!%*_+`'~")) }
+  def transportParam = rule { "transport=" ~ ("udp" | "tcp" | "sctp" | "tls" | token) }
+  def userParam = rule { "user=" ~ ( "phone" | "ip" | token) }
+  def methodParam = rule { "method=" ~ method }
+  def ttl = rule { (1 to 3).times(CharPredicate.Digit) }
+  def ttlParam = rule { "ttl=" ~ ttl }
+  def maddrParam  = rule { "maddr=" ~ host }
+  def param  =  rule { CharPredicate("[]/:&+$") | unreserved | escaped }
+  def paramNameValue = rule { oneOrMore(param) }
+  def otherParam = rule { paramNameValue ~ optional('=' ~ paramNameValue) }
+  def uriParameter = rule { transportParam | userParam | methodParam | ttlParam | maddrParam | "lr" | otherParam }
+  def uriParameters = rule { zeroOrMore(';' ~ uriParameter) }
+
   //def SipUri = rule { "sip:" ~ optional(userInfo) ~ hostAndPort ~ uriParameters ~ optional(headers)}
   //def SipsUri = rule { "sips:" ~ optional(userInfo) ~ hostAndPort ~ uriParameters ~ optional(headers)}
 }
